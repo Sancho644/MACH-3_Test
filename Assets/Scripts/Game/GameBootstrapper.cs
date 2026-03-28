71622eabbec822e4049dd864f36b92302b5e263b
Alexandr <rfst@mail.ru>
2026-03-28 17:27:41 +0700

    fix GameBootstrapper

--- Assets/Scripts/Game/GameBootstrapper.cs
+++ Assets/Scripts/Game/GameBootstrapper.cs
@@ -1,4 +1,4 @@
-using Game.States;
+﻿using Game.States;
 using Scenes;
 using UnityEngine;
 
@@ -6,14 +6,31 @@ namespace Game
 {
     public class GameBootstrapper : MonoBehaviour, ICoroutineRunner
     {
+        private static GameBootstrapper _instance;
         private GameStateMachine _gameStateMachine;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Bootstrap()
+        {
+            if (_instance != null) return;
+
+            var go = new GameObject("[GameBootstrapper]");
+            _instance = go.AddComponent<GameBootstrapper>();
+            DontDestroyOnLoad(go);
+        }
 
         private void Awake()
         {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = this;
+
             _gameStateMachine = new GameStateMachine(this);
             _gameStateMachine.Enter<BootstrapState>();
-
-            DontDestroyOnLoad(this);
         }
     }
 }
