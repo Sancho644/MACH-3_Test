68943e283da1f4f3986de733cf969be9e4fa779b
Alexandr <rfst@mail.ru>
2026-04-02 21:33:10 +0700

    create animation tweens

---
+++ Assets/Scripts/UI/Animations/ExplosionTween.cs
@@ -0,0 +1,42 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Animations
+{
+    [RequireComponent(typeof(Image))]
+    public class ExplosionTween : MonoBehaviour
+    {
+        [SerializeField] private float duration = 0.18f;
+        [SerializeField] private float scaleMultiplier = 1.25f;
+        [SerializeField] private Ease ease = Ease.OutQuad;
+        [SerializeField] private Image image;
+        
+        private Sequence _sequence;
+
+        public Sequence Play()
+        {
+            _sequence?.Kill(true);
+            
+            _sequence = DOTween.Sequence();
+            var targetScale = transform.localScale * scaleMultiplier;
+            
+            _sequence.Append(transform.DOScale(targetScale, duration).SetEase(ease));
+            _sequence.Append(image.DOFade(0f, duration));
+            _sequence.Play();
+            
+            return _sequence;
+        }
+
+        public Tween GetTween()
+        {
+            var targetScale = transform.localScale * scaleMultiplier;
+            return transform.DOScale(targetScale, duration).SetEase(ease);
+        }
+        
+        private void OnDestroy()
+        {
+            _sequence?.Kill();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Animations/MoveTween.cs
@@ -0,0 +1,39 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace UI.Animations
+{
+    public class MoveTween : MonoBehaviour
+    {
+        [SerializeField] private float duration = 0.2f;
+        [SerializeField] private Ease ease = Ease.OutQuad;
+
+        private Sequence _sequence;
+
+        public Sequence Play(Vector3 targetPosition)
+        {
+            _sequence?.Kill(true);
+
+            _sequence = DOTween.Sequence();
+            _sequence.Append(transform.DOMove(targetPosition, duration).SetEase(ease));
+            _sequence.Play();
+
+            return _sequence;
+        }
+
+        public Tween GetTween(Vector3 targetPosition)
+        {
+            return transform.DOMove(targetPosition, duration).SetEase(ease);
+        }
+
+        public Tween GetTween(Vector3 targetPosition, float tweenDuration, Ease tweenEase)
+        {
+            return transform.DOMove(targetPosition, tweenDuration).SetEase(tweenEase);
+        }
+
+        private void OnDestroy()
+        {
+            _sequence?.Kill();
+        }
+    }
+}
