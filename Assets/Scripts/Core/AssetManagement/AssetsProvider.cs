d356ca19822296f8db87f39a203a6bed78dc0c28
Alexandr <rfst@mail.ru>
2026-04-02 14:49:33 +0700

    create asset management

---
+++ Assets/Scripts/Core/AssetManagement/AssetsProvider.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Core.AssetManagement
+{
+    public class AssetsProvider : IAssets
+    {
+        private readonly Dictionary<string, AsyncOperationHandle> _completeCache = new();
+        private readonly Dictionary<string, List<AsyncOperationHandle>> _handles = new();
+
+        public void Instantiate()
+        {
+            Addressables.InitializeAsync();
+        }
+
+        public async Task<T> Load<T>(AssetReference assetReference) where T : class
+        {
+            if (_completeCache.TryGetValue(assetReference.AssetGUID, out AsyncOperationHandle completedHandle))
+                return completedHandle.Result as T;
+
+            return await RunWithCacheOnComplete(
+                Addressables.LoadAssetAsync<T>(assetReference), assetReference.AssetGUID);
+        }
+
+        public async Task<T> Load<T>(string address) where T : class
+        {
+            if (_completeCache.TryGetValue(address, out AsyncOperationHandle completeHandle))
+                return completeHandle.Result as T;
+
+            return await RunWithCacheOnComplete(
+                Addressables.LoadAssetAsync<T>(address), address);
+        }
+
+        public Task<GameObject> Instantiate(string address) =>
+            Addressables.InstantiateAsync(address).Task;
+
+        public Task<GameObject> Instantiate(string address, Vector3 at) =>
+            Addressables.InstantiateAsync(address, at, Quaternion.identity).Task;
+
+        public Task<GameObject> Instantiate(string address, Transform under) =>
+            Addressables.InstantiateAsync(address, under).Task;
+
+        public void CleanUp()
+        {
+            foreach (List<AsyncOperationHandle> resourceHandles in _handles.Values)
+            foreach (AsyncOperationHandle handle in resourceHandles)
+                Addressables.Release(handle);
+
+            _completeCache.Clear();
+            _handles.Clear();
+        }
+
+        private async Task<T> RunWithCacheOnComplete<T>(AsyncOperationHandle<T> handle, string cacheKey) where T : class
+        {
+            handle.Completed += completeHandle => { _completeCache[cacheKey] = completeHandle; };
+
+            AddHandle(cacheKey, handle);
+
+            return await handle.Task;
+        }
+
+        private void AddHandle<T>(string key, AsyncOperationHandle<T> handle) where T : class
+        {
+            if (!_handles.TryGetValue(key, out List<AsyncOperationHandle> resourceHandle))
+            {
+                resourceHandle = new List<AsyncOperationHandle>();
+                _handles[key] = resourceHandle;
+            }
+
+            resourceHandle.Add(handle);
+        }
+    }
+}
---
+++ Assets/Scripts/Core/AssetManagement/IAssets.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Core.AssetManagement
+{
+    public interface IAssets
+    {
+        public Task<GameObject> Instantiate(string address);
+        public Task<GameObject> Instantiate(string address, Vector3 at);
+        public Task<GameObject> Instantiate(string address, Transform under);
+        public Task<T> Load<T>(AssetReference assetReference) where T : class;
+        public Task<T> Load<T>(string address) where T : class;
+        public void CleanUp();
+        public void Instantiate();
+    }
+}
