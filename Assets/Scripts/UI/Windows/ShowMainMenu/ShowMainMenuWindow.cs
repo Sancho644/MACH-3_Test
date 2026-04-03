b190fd55fdf400cc19dc69152883dab4e5d5805d
Alexandr <rfst@mail.ru>
2026-04-03 23:49:23 +0700

    create ShowMainMenuWindow

---
+++ Assets/Scripts/UI/Windows/ShowMainMenu/ShowMainMenuWindow.cs
@@ -0,0 +1,40 @@
+﻿using Game.Services.GameEvents;
+using GameEvents;
+using UI.Windows.MainMenu;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Windows.ShowMainMenu
+{
+    public class ShowMainMenuWindow : AbstractWindow
+    {
+        [SerializeField] private Button goMenuButton;
+        [SerializeField] private Button stayButton;
+
+        private IGameEventsDispatcher _gameEventsDispatcher;
+
+        public void Initialize(IGameEventsDispatcher gameEventsDispatcher)
+        {
+            _gameEventsDispatcher = gameEventsDispatcher;
+
+            goMenuButton.onClick.AddListener(OnGoMenu);
+            stayButton.onClick.AddListener(OnStay);
+        }
+
+        private void OnDestroy()
+        {
+            goMenuButton.onClick.RemoveListener(OnGoMenu);
+            stayButton.onClick.RemoveListener(OnStay);
+        }
+
+        private void OnGoMenu()
+        {
+            _gameEventsDispatcher.Dispatch(new GameActionEvent(GameActionType.MainMenu));
+        }
+
+        private void OnStay()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
