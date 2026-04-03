47b1749eef306ac0d7dc76303299c78a8d3bafa7
Alexandr <rfst@mail.ru>
2026-04-03 23:49:48 +0700

    create LowScoreWindow

---
+++ Assets/Scripts/UI/Windows/LowScore/LowScoreWindow.cs
@@ -0,0 +1,32 @@
+﻿using Game.Services.GameEvents;
+using GameEvents;
+using UI.Windows.MainMenu;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Windows.LowScore
+{
+    public class LowScoreWindow : AbstractWindow
+    {
+        [SerializeField] private Button button;
+
+        private IGameEventsDispatcher _gameEventsDispatcher;
+
+        public void Init(IGameEventsDispatcher gameEventsDispatcher)
+        {
+            _gameEventsDispatcher = gameEventsDispatcher;
+            
+            button.onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnDestroy()
+        {
+            button.onClick.RemoveListener(OnButtonClick);
+        }
+
+        private void OnButtonClick()
+        {
+            _gameEventsDispatcher.Dispatch(new GameActionEvent(GameActionType.MainMenu));
+        }
+    }
+}
