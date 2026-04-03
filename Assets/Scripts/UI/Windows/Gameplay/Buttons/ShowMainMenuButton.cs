3c363728bf39d0750b8be98fa4e0352cb42e756a
Alexandr <rfst@mail.ru>
2026-04-03 23:47:47 +0700

    create ShowMainMenuButton

---
+++ Assets/Scripts/UI/Windows/Gameplay/Buttons/ShowMainMenuButton.cs
@@ -0,0 +1,34 @@
+﻿using UI.Services.Factory;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Windows.Gameplay.Buttons
+{
+    [RequireComponent(typeof(Button))]
+    public class ShowMainMenuButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+
+        private IUIFactoryService _uiFactoryService;
+
+        public void Initialize(IUIFactoryService uiFactoryService)
+        {
+            _uiFactoryService = uiFactoryService;
+        }
+
+        private void Awake()
+        {
+            button.onClick.AddListener(OnClick);
+        }
+
+        private void OnDestroy()
+        {
+            button.onClick.RemoveListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            _uiFactoryService.CreateShowMainMenuWindow();
+        }
+    }
+}
