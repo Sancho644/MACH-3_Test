96edcea32efa9240d2ae59bef8ff75039f9e5ee9
Alexandr <rfst@mail.ru>
2026-04-13 20:06:31 +0700

    create ConfirmExitWindow

---
+++ Assets/Scripts/UI/Windows/ConfirmExit/ConfirmExitWindow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Windows.ConfirmExit
+{
+    public class ConfirmExitWindow : AbstractWindow
+    {
+        [SerializeField] private Button exitButton;
+        [SerializeField] private Button stayButton;
+
+        private void Awake()
+        {
+            exitButton.onClick.AddListener(OnExit);
+            stayButton.onClick.AddListener(OnStay);
+        }
+
+        private void OnDestroy()
+        {
+            exitButton.onClick.RemoveListener(OnExit);
+            stayButton.onClick.RemoveListener(OnStay);
+        }
+
+        private void OnStay()
+        {
+            Destroy(gameObject);
+        }
+
+        private void OnExit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
+    }
+}
